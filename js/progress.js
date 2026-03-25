b178f2420ad75e4be6b6855cee2fdfb3dd1d11e0
Anikait Kulkarni <kulkarnianikait@gmail.com>
2026-03-25 12:13:24 +0530

    updated contact page

--- js/progress.js
+++
@@ -1,9 +0,0 @@
-const progressBar = document.querySelector(".progress-bar");
-if (progressBar) {
-    window.addEventListener("scroll", () => {
-        const scrollTop = document.documentElement.scrollTop;
-        const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-        const progress = (scrollTop / height) * 100;
-        progressBar.style.width = progress + "%";
-    });
-}
